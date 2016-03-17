c0af6e1603a08d8a0eb9cc1e353b3f15aa4768d7
jrburke <jrburke@gmail.com>
2016-03-17 10:01:22 -0700

    Relates to #13, update alameda to pass requirejs 2.2 tests

--- alameda.js
+++ alameda.js
@@ -141,14 +141,13 @@ var requirejs, require, define;
           ary.splice(i, 1);
           i -= 1;
         } else if (part === '..') {
-          if (i === 1 && (ary[2] === '..' || ary[0] === '..')) {
-            // End of the line. Keep at least one non-dot
-            // path segment at the front so it can be mapped
-            // correctly to disk. Otherwise, there is likely
-            // no path mapping for a path starting with '..'.
-            // This can still fail, but catches the most reasonable
-            // uses of ..
-            break;
+          // If at the start, or previous value is still ..,
+          // keep them so that when converted to a path it may
+          // still work when converted to a path, even though
+          // as an ID it is less than ideal. In larger point
+          // releases, may be better to just kick out an error.
+          if (i === 0 || (i === 1 && ary[2] === '..') || ary[i - 1] === '..') {
+            continue;
           } else if (i > 0) {
             ary.splice(i - 1, 2);
             i -= 2;
@@ -175,37 +174,33 @@ var requirejs, require, define;
         map = config.map,
         starMap = map && map['*'];
 
-      // Adjust any relative paths.
-      if (name && name.charAt(0) === '.') {
-        // If have a base name, try to normalize against it,
-        // otherwise, assume it is a top-level require that will
-        // be relative to baseUrl in the end.
-        if (baseName) {
-          // Convert baseName to array, and lop off the last part,
-          // so that . matches that 'directory' and not name of the baseName's
-          // module. For instance, baseName of 'one/two/three', maps to
-          // 'one/two/three.js', but we want the directory, 'one/two' for
-          // this normalization.
-          normalizedBaseParts = baseParts.slice(0, baseParts.length - 1);
-          name = name.split('/');
-          lastIndex = name.length - 1;
 
-          // If wanting node ID compatibility, strip .js from end
-          // of IDs. Have to do this here, and not in nameToUrl
-          // because node allows either .js or non .js to map
-          // to same file.
-          if (config.nodeIdCompat && jsSuffixRegExp.test(name[lastIndex])) {
-            name[lastIndex] = name[lastIndex].replace(jsSuffixRegExp, '');
-          }
+      //Adjust any relative paths.
+      if (name) {
+        name = name.split('/');
+        lastIndex = name.length - 1;
 
-          name = normalizedBaseParts.concat(name);
-          trimDots(name);
-          name = name.join('/');
-        } else if (name.indexOf('./') === 0) {
-          // No baseName, so this is ID is resolved relative
-          // to baseUrl, pull off the leading dot.
-          name = name.substring(2);
+        // If wanting node ID compatibility, strip .js from end
+        // of IDs. Have to do this here, and not in nameToUrl
+        // because node allows either .js or non .js to map
+        // to same file.
+        if (config.nodeIdCompat && jsSuffixRegExp.test(name[lastIndex])) {
+          name[lastIndex] = name[lastIndex].replace(jsSuffixRegExp, '');
         }
+
+        // Starts with a '.' so need the baseName
+        if (name[0].charAt(0) === '.' && baseParts) {
+          //Convert baseName to array, and lop off the last part,
+          //so that . matches that 'directory' and not name of the baseName's
+          //module. For instance, baseName of 'one/two/three', maps to
+          //'one/two/three.js', but we want the directory, 'one/two' for
+          //this normalization.
+          normalizedBaseParts = baseParts.slice(0, baseParts.length - 1);
+          name = normalizedBaseParts.concat(name);
+        }
+
+        trimDots(name);
+        name = name.join('/');
       }
 
       // Apply map config if available.
@@ -808,7 +803,16 @@ var requirejs, require, define;
         if (plugin && plugin.normalize) {
           name = plugin.normalize(name, makeNormalize(relName));
         } else {
-          name = normalize(name, relName, applyMap);
+          // If nested plugin references, then do not try to
+          // normalize, as it will not normalize correctly. This
+          // places a restriction on resourceIds, and the longer
+          // term solution is not to normalize until plugins are
+          // loaded and all normalizations to allow for async
+          // loading of a loader plugin. But for now, fixes the
+          // common uses. Details in requirejs#1131
+          name = name.indexOf('!') === -1 ?
+                   normalize(name, relName, applyMap) :
+                   name;
         }
       } else {
         name = normalize(name, relName, applyMap);
